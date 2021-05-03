e345acab99337bff5cf8a1855113dc884f69ef82
Diana Bowler <diana.bowler@idiv.de>
2021-05-03 21:49:00 +0200

    put different species on the same plot

--- simulations/samplingBias_speciesEffects.R
+++ simulations/samplingBias_speciesEffects.R
@@ -5,6 +5,59 @@ library(ggthemes)
 
 source('simulations/spatialBias_functions.R')
 
+#### Species negatively associated with urban change ####
+
+outputNC <- ldply(1:1000,function(i){
+  
+  df <- generateData(beta1=-3)
+  next_df <- extendData(df,beta1=-3,change="no_change")
+  #get proportion of sites observed to be occupied
+  getObsProp(next_df)
+  
+})
+q1 <- plotObsProp_P(outputNC,mytitle="No urban change")
+g1 <- plotObsChange(outputNC)
+
+outputUC <- ldply(1:1000,function(i){
+  
+  df <- generateData(beta1=-3)
+  next_df <- extendData(df,beta1=-3,change="uniform_change")
+  #get proportion of sites observed to be occupied
+  getObsProp(next_df)
+  
+})
+q2 <- plotObsProp_P(outputUC,mytitle="Uniform urban change")
+g2 <- plotObsChange(outputUC)
+
+outputCC <- ldply(1:1000,function(i){
+  
+  df <- generateData(beta1=-3)
+  next_df <- extendData(df,beta1=-3,change="clustered_change")
+  #get proportion of sites observed to be occupied
+  getObsProp(next_df)
+  
+})
+q3 <- plotObsProp_P(outputCC,mytitle="Clustered urban change")
+g3 <- plotObsChange(outputCC)
+
+
+plot1 <- plot_grid(q1,q2,q3,nrow=1)
+plot2 <- plot_grid(g1,g2,g3,nrow=1)
+
+plot_grid(plot1,plot2,ncol=1,align='v',
+          labels=c("A","B"),
+          vjust=1)
+
+ggsave("plots/SOM/Obs_change_negativeSpecies.png",width=10.5,height=6)
+
+
+#combine all outputs
+outputNC$Envion_change <- "no change"
+outputUC$Envion_change <- "uniform change"
+outputCC$Envion_change <- "clustered change"
+outputNegative <- rbind(outputNC,outputUC,outputCC) 
+outputNegative$Species <- "Negative"
+
 ### Species positively associated with urban cover #####
 
 ### observed change ###################################
@@ -52,6 +105,13 @@ plot_grid(plot1,plot2,ncol=1,align='v',
 
 ggsave("plots/SOM/Obs_change_positiveSpecies.png",width=10.5,height=6)
 
+#combine all outputs
+outputNC$Envion_change <- "no change"
+outputUC$Envion_change <- "uniform change"
+outputCC$Envion_change <- "clustered change"
+outputPositive <- rbind(outputNC,outputUC,outputCC) 
+outputPositive$Species <- "Positive"
+
 ### Species neutrally associated with urban cover #####
 
 ### observed change ###################################
@@ -98,5 +158,45 @@ plot_grid(plot1,plot2,ncol=1,align='v',
           vjust=1)
 
 ggsave("plots/SOM/Obs_change_neutralSpecies.png",width=10.5,height=6)
+
+#combine all outputs
+outputNC$Envion_change <- "no change"
+outputUC$Envion_change <- "uniform change"
+outputCC$Envion_change <- "clustered change"
+outputNeutral <- rbind(outputNC,outputUC,outputCC) 
+outputNeutral$Species <- "Neutral"
+
+
+#### combine all ####
+
+output <- rbind(outputNeutral,outputNegative,outputPositive)
+output$Envion_change <- factor(output$Envion_change,
+                               levels=c("no change","uniform change","clustered change"))
+
+output$simNu <- 1:nrow(output)
+output_melt <- reshape2::melt(output,id=c("simNu","Envion_change","Species"))
+output_melt$scenario <- sapply(as.character(output_melt$variable),function(x){
+  strsplit(x,"_")[[1]][1]})
+output_melt$time <- sapply(as.character(output_melt$variable),function(x){
+  strsplit(x,"_")[[1]][2]})
+output_cast <- reshape2::dcast(output_melt,simNu+scenario + Envion_change+Species~time,value.var="value")
+output_cast$Difference2 <- output_cast$Time2/output_cast$Time1
+
+
+ggplot(output_cast,aes(x=scenario,y=Difference2))+
+  geom_violin(aes(fill=Envion_change, colour=Envion_change),
+              position="dodge",draw_quantiles=c(0.25,0.5,0.75),
+              alpha=0.5)+
+  theme_few()+
+  scale_x_discrete("Sampling scenario", labels = c("Visit1" = "Full","Visit2" = "Random",
+                                                   "Visit3" = "Bias","Visit4" = "Bias+"))+
+  ylab("Occupancy change")+
+  facet_wrap(~Species)+
+  scale_fill_brewer("Urban change")+
+  scale_color_brewer("Urban change")+
+  geom_hline(yintercept=1,linetype="dashed")
+
+
+ggsave("plots/Obs_change_speciesPref.png",width=9,height=3)
 
 ### end ##################################################
