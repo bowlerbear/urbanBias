66570587ca7649a332ea26483859184444a2a50d
Diana Bowler <diana.bowler@idiv.de>
2021-11-08 14:06:37 +0100

    remember to load sf library

--- empirical-data/HPC_testUrbanBias.R
+++ empirical-data/HPC_testUrbanBias.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(broom)
 library(mgcv)
+library(sf)
 
 ### environ datasets ####
 
@@ -26,7 +27,7 @@ getAnnualBiasSpace <- function(myfolder, mytaxa){
     
     tempData <- samplingIntensity[samplingIntensity$Year == y,]
     
-    randomSample <- sample(1:nrow(tempData),5000)
+    randomSample <- sample(1:nrow(tempData),100)
     dat <- tempData[randomSample,]
     #model1 <- glm(Visited ~ urban, family= binomial, data = dat)
     #model_coefs <- broom::tidy(model1)
