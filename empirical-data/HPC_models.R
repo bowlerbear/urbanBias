879abdae8ea5b7ba055826a19e74c7ab27d6377e
Diana Bowler <diana.bowler@idiv.de>
2021-11-18 15:42:24 +0100

    checking annual models

--- empirical-data/HPC_models.R
+++ empirical-data/HPC_models.R
@@ -5,10 +5,19 @@ modelFiles <- list.files("model-outputs")
 
 ### annual urban #######
 
-modelFiles_subset <- modelFiles %>% str_subset("annualBiasSpace_") 
+modelFiles_subset <- modelFiles %>% 
+  str_subset("annualBiasSpace_") %>% 
+  str_subset("PAarea", negate=TRUE)
 
-#glm plots
-library(ggplot2)
+allModels <- lapply(modelFiles_subset, function(x){
+  temp <- readRDS(paste("model-outputs",x,sep="/"))
+  temp$Files <- x
+  temp$dataset <- strsplit(temp$Files,"_")[[1]][2]
+  temp$taxa <- strsplit(temp$Files,"_")[[1]][3]
+  temp$taxa <- gsub(".rds","",temp$taxa)
+  return(temp)
+})
+allModels <- do.call(rbind,allModels)
 
 ggplot(subset(allModels,model=="glm"))+
   geom_crossbar(aes(x=Year, y = estimate, ymax=upper, ymin = lower))+
@@ -20,18 +29,29 @@ ggplot(subset(allModels,model=="spaMM"))+
   geom_hline(yintercept=0,linetype="dashed")+
   facet_wrap(dataset~taxa,scales="free")
 
-#run again for for longer and more iterations
-ggplot(subset(allModels,model=="spaMM" & Year >2010))+
-  geom_crossbar(aes(x=Year, y = estimate, ymax=upper, ymin = lower))+
-  geom_hline(yintercept=0,linetype="dashed")+
-  facet_wrap(dataset~taxa,scales="free")
-
-
 ### urban interaction ####
 
 modelFiles_subset <- modelFiles %>% 
   str_subset("interactionBiasSpace_") %>% 
   str_subset("PAarea", negate=TRUE)
+
+allModels <- lapply(modelFiles_subset, function(x){
+  temp <- readRDS(paste("model-outputs",x,sep="/"))
+  temp$Files <- x
+  temp$dataset <- strsplit(temp$Files,"_")[[1]][2]
+  temp$taxa <- strsplit(temp$Files,"_")[[1]][3]
+  temp$taxa <- gsub(".rds","",temp$taxa)
+  return(temp)
+})
+allModels <- do.call(rbind,allModels)
+
+
+### annual PA area ####
+
+### PA area interaction ####
+
+modelFiles_subset <- modelFiles %>% 
+  str_subset("interactionBiasSpace_PAarea") 
 
 allModels <- lapply(modelFiles_subset, function(x){
   temp <- readRDS(paste("model-outputs",x,sep="/"))
